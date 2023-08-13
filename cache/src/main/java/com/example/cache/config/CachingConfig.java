e115a96f02318ec2c983a58c2a4252b58ce5c37f
seonghoo1217 <seonghoo1217@naver.com>
2023-08-13 22:51:16 +0900

    feat : add cache manager config

---
+++ cache/src/main/java/com/example/cache/config/CachingConfig.java
@@ -0,0 +1,23 @@
+package com.example.cache.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+@EnableCaching
+@Configuration
+public class CachingConfig {
+    @Bean
+    public CacheManager cacheManager(){
+        ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager();
+        cacheManager.setAllowNullValues(false);
+        cacheManager.setCacheNames(List.of("members"));
+        return cacheManager;
+    }
+}
