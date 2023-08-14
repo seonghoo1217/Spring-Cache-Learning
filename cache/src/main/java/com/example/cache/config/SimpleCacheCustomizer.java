d51eaba85e801e33ccf107e473d2ac1c1b719ebe
seonghoo1217 <seonghoo1217@naver.com>
2023-08-15 02:08:16 +0900

    feat : add cache customizer

---
+++ cache/src/main/java/com/example/cache/config/SimpleCacheCustomizer.java
@@ -0,0 +1,13 @@
+package com.example.cache.config;
+
+import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleCacheCustomizer implements CacheManagerCustomizer<ConcurrentMapCacheManager> {
+    @Override
+    public void customize(ConcurrentMapCacheManager cacheManager) {
+        cacheManager.setAllowNullValues(false);
+    }
+}
